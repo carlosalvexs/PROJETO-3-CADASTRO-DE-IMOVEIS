6cd7cd1b8376c44fd48a89066fd1761300e88056
carlos <carlos.teixeira56@aluno.ce.gov.br>
2024-10-28 16:59:49 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let property = [];
+ let continue = true;
+
+ while (continuar) {
+    let menu = '\n cadastro de imoveis:\n';
+    menu += "quantiade de ${propety.length} imoveis cadastrados\n 1 SALVAR UM NOVO IMOVEL \n 2 LITAR IMOVEIS SALVOS \n 3 SAIR "
+ let option = prompt(menu)
+
+if (option === "1"){
+    let nomeDoImovel = prompt ("nome do imovel:")
+    property.push(nomeDoImovel)
+prompt'{$nomeDoImovel'} adicionado ao menu
+}}
